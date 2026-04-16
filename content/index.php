437a4a740512e886ac85d4c5cfaada99324643de
sabilaputribilqis23-lgtm <sabilaputribilqis23@gmail.com>
2026-04-16 17:25:58 +0700

    Add files via upload

---
+++ content/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, instal-scale=1.0">
+    <title>Pertemuan 2</title>
+</head>
+<body>
+    
+<table border="1" width="100%">
+    <tr>
+        <!-- Kolom 1: Gambar -->
+         <td width="10%">
+            <img src="https://unsiq.ac.id/img/Logounsiq.jpg" alt="Unsiq", width="100%", height="100%">
+         </td>
+
+        <!-- Kolom 2: Heading + Teks -->
+         <td>
+            <h2>Selamat datang di Mata Kuliah Pemrograman Web</h2>
+            <h3>Pertemuan 2: HTML Dasar</h3>
+         </td>
+    </tr>
+
+</table>
+
+<p align="center">Mata Kuliah ini memperkenalkan dasar-dasar pemrogaman web menggunakan HTML, CSS, dan JavaScript</p>
+<p align="center">Pada pertemuan ini, kita fokus pada struktur dan elemendasar HTML.</p>
+
+<hr>
+
+<p align="center">HTML adalah bahasa markup yang digunakanuntuk membuat struktur halaman web. Dengan HTML, kita bisa menampilkan teks, gambar, dan link secara terorganisir. Untuk informasi lengkap dari HTML dapat kunjungi link berikut: <a href="https://share.google/fFInTripw3eTCGXop">Penjelasan HTML</a>
+</p>
+
+<h3>Topik Utama Pertemuan ini:</h3>
+<ul>
+    <li>Struktur HTML</li>
+    <li>Heading dan Paragraf</li>
+    <li>List(Unordered dan Ordered)</li>
+    <li>Link dan Gambar</li>
+    <li>Tabel</li>
+</ul>
+
+<h3>Langkah Praktikum:</h3>
+<ol>
+    <li>Buat file index.php</li>
+    <li>Tulis kode HTML</li>
+    <li>Simpan dan buka di browser</li>
+        <ul>
+            <li>buka exampp</li>
+            <li>nyalakan server</li>
+            <li>Buka browser dengan url localhost/...</li>
+        </ul>
+    <li>Verifikasi hasil</li>
+</ol>
+</body>
+</html>
